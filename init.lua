16ea75b0b28b9f7de888c9e1cc9cf5cc0c1f7388
joel-a4817 <joel.ag789@gmail.com>
2025-12-28 09:09:36 +0800

    Initialize configuration for dupes, git, and recycle-bin

---
+++ init.lua
@@ -0,0 +1,37 @@
+#
+### dupes
+#
+
+require("dupes"):setup {
+	-- Global settings
+	save_op = false,        -- Save results to file by default
+	-- auto_confirm = true, -- Skip confirmation for apply (use with caution!)
+	
+	profiles = {
+		-- Interactive mode: recursively scan and display duplicates
+		interactive = {
+			args = { "-r" },
+		},
+		-- Apply mode: recursively scan and DELETE duplicates
+		apply = {
+			args = { "-r", "-N", "-d" },
+			save_op = true,  -- Save results before deletion
+		},
+		-- Custom profile example (uncomment to use)
+		-- custom = {
+		-- 	args = { "-r", "-s", },  -- Your custom jdupes flags
+		-- },
+	},
+}
+
+#
+## git
+#
+
+require("git"):setup()
+
+#
+### recycle-bin
+#
+
+require("recycle-bin"):setup()
